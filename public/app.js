516ce7dd1a4027fc05aa207b287c9de22e62c2b5
KuzeClinic <103870716+KuzeClinic@users.noreply.github.com>
2022-04-19 17:26:19 +0900

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,28 @@
+const menu = document.querySelector("#mobile");
+const links = document.querySelector(".nav");
+const overlay = document.querySelector(".overlay");
+const mobilemenu = () => {
+  menu.classList.toggle("activated");
+  links.classList.toggle("active");
+  overlay.classList.toggle("activated");
+};
+menu.addEventListener("click", mobilemenu);
+
+function hide() {
+  if (window.innerWidth <= 768) {
+    menu.classList.remove("activated");
+    links.classList.remove("active");
+    overlay.classList.remove("activated");
+  }
+}
+
+const list = document.querySelector(".navlist");
+const hidethis = () => {
+  const menuBars = document.querySelector(".active");
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.remove("activated");
+    links.classList.remove("active");
+    overlay.classList.remove("activated");
+  }
+};
+list.addEventListener("click", hidethis);
